9d76eb67270100e3b6623ea40a026df2d77a0a8f
AugustusWendell <augustuswendell@gmail.com>
2018-06-12 02:49:53 -0400

    06_12_18

---
+++ SpatioScholar_Agent/Assets/CSV_output.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System;
+using UnityEngine.AI;
+
+public class CSV_output : MonoBehaviour
+{
+    private List<string[]> rowData = new List<string[]>();
+
+
+    // Use this for initialization
+    void Start()
+    {
+        //Calls the Save() method to save out the CSV file
+        //Save();
+    }
+
+    public void Save()
+    {
+        print("CSV Save Method Called");
+        // Creating First row of titles manually.
+        string[] rowDataTemp = new string[5];
+        rowDataTemp[0] = "ID_Number";
+        rowDataTemp[1] = "Birth_Location";
+        rowDataTemp[2] = "Current_Location";
+        rowDataTemp[3] = "Current_Vector";
+        rowDataTemp[4] = "Target";
+        rowData.Add(rowDataTemp);
+
+
+        /* example calls to retrieve variables and call methods in other components added to the same object
+        GetComponent(Gravity).SetGravityRange(1000);
+        GetComponent<Example>().AgentList;
+        */
+
+        // You can add up the values in as many cells as you want.
+        //for (int i = 0; i < GetComponent<Example>().AgentList.Count; i++)
+        for (int i = 1; i < 3; i++)
+        {
+            //debug
+            print("CSV Save data method : finding agent number " + i);
+            //debug object being queried
+            print("CSV Save data method : finding information from agent " + GetComponent<Example>().AgentList[i]);
+            rowDataTemp = new string[5];
+            //rowDataTemp[0] = "Sushanta" + i; // name
+            rowDataTemp[0] = "" + i; // ID_Number
+            rowDataTemp[1] = GetComponent<Example>().AgentList[i].transform.position.ToString(); // Birth_Location
+            rowDataTemp[2] = GetComponent<Example>().AgentList[i].transform.position.ToString(); // Current_Location
+            rowDataTemp[3] = GetComponent<Example>().AgentList[i].velocity.ToString(); // Current_Vector
+            rowDataTemp[4] = GetComponent<Example>().AgentList[i].destination.ToString(); // Target
+            rowData.Add(rowDataTemp);
+        }
+
+        string[][] output = new string[rowData.Count][];
+
+        for (int i = 0; i < output.Length; i++)
+        {
+            output[i] = rowData[i];
+        }
+
+        int length = output.GetLength(0);
+        string delimiter = ",";
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int index = 0; index < length; index++)
+            sb.AppendLine(string.Join(delimiter, output[index]));
+
+
+        string filePath = getPath();
+
+        StreamWriter outStream = System.IO.File.CreateText(filePath);
+        outStream.WriteLine(sb);
+        outStream.Close();
+    }
+
+
+   
+    
+
+    // Following method is used to retrive the relative path as device platform
+    private string getPath()
+    {
+#if UNITY_EDITOR
+        return Application.dataPath + "/CSV/" + "Saved_data.csv";
+#elif UNITY_ANDROID
+        return Application.persistentDataPath+"Saved_data.csv";
+#elif UNITY_IPHONE
+        return Application.persistentDataPath+"/"+"Saved_data.csv";
+#else
+        return Application.dataPath +"/"+"Saved_data.csv";
+#endif
+    }
+}
+
---
+++ SpatioScholar_Agent/Assets/PlayerController.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour {
+    public Camera cam;
+    public NavMeshAgent agent;
+    public bool vector_render;
+    public bool attributes_render;
+    public bool attributes_visible;
+
+        // Use this for initialization
+        void Start () {
+        //debug
+        print("PlayerController initialization");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //Test RayTrace Code
+        int raynumber = 10;
+        //Vector3 RayDirection = new Vector3(1, 0, 1);
+        Vector3 RayDirection = new Vector3(Random.Range(0f,1f), 0, Random.Range(0f, 1f));
+        for (int i = 0; i < raynumber; i++)
+        {
+            Debug.DrawRay(transform.position, RayDirection * 50, Color.green);
+            //rotate Ray Direction Vector3
+            RayDirection = Quaternion.Euler(0, (360/raynumber), 0) * RayDirection;
+        }
+
+        //listener to move to the raycast location
+        /*
+		 if (Input.GetMouseButtonDown(0))
+        {
+            print("PlayerController Button Down Registered");
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                //Move the Agent
+                agent.SetDestination(hit.point);
+                print("PlayerController moving agent based on raycast");
+            }
+
+        }
+        */
+
+
+        //Test for Vector Flag to turn on vector line renderer
+        if (vector_render == true) {
+
+            //invoke the LineRenderer to render the direction vector for this agent
+            LineRenderer lineRenderer = this.GetComponent<LineRenderer>();
+            lineRenderer.SetVertexCount(2);
+            lineRenderer.SetPosition(0, transform.position);
+            lineRenderer.SetPosition(1, transform.forward * 3 + transform.position);
+        }
+
+        //make sure atributes visible is turned off
+        if (attributes_render = false)
+        {
+            if (attributes_visible = true)
+            {
+                attributes_visible = false;
+            }
+        }
+
+        //Test for Attribute Render Flag to turn on Canvas
+        if (attributes_render = true)
+        {
+            if (attributes_visible = false){
+                attributes_visible = true;
+            }
+
+            //make the canvas visible
+            //Canvas myCanvas = this.GetComponent<LineRenderer>();
+
+        }
+	}
+
+    public void ToggleVector()
+    {
+        //debug
+        print("Setting Vector Render False");
+        vector_render = false;
+    }
+
+    public void Sky_Exposure()
+    {
+        //run a test to see what percentage of sky is hit
+        int raynumber = 10;
+        int RaysHit = 0;
+        for (int i = 0; i < raynumber; i++)
+        {
+            Debug.DrawRay(transform.position, new Vector3(1, 1, 1) * 50, Color.green);
+            RaycastHit objectHit;
+            // Shoot raycast
+            if (Physics.Raycast(transform.position, new Vector3(1,1,1) , out objectHit, 50))
+            {
+                //Debug.DrawRay(transform.position, fwd * 50, Color.green);
+                RaysHit++;
+            }
+        }
+
+    }
+}
